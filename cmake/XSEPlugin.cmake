6ff5846ee9009833f7f588c3ff6bae14449dd352
Gistix <gistix.md@hotmail.com>
2026-06-11 04:51:04 -0300

    fix spdlog linker errors

--- cmake/XSEPlugin.cmake
+++ cmake/XSEPlugin.cmake
@@ -107,54 +107,7 @@ elseif(BUILD_FALLOUT4)
 	set(CommonLibSharedName "CommonLibShared")
 	set(CommonLibSharedTarget CommonLibShared)
 
-	# Hardcoded as release for now
-	set(XMAKE_MODE "release")
-
-	set(CommonLibBuildPath "${CommonLibFullPath}/build/windows/x64/${XMAKE_MODE}")
-
-	# Find xmake
-	find_program(XMAKE_EXE 
-		NAMES xmake xmake.exe
-		HINTS "C:/Program Files/xmake/"
-	)
-
-	if(NOT XMAKE_EXE OR XMAKE_EXE STREQUAL "XMAKE_EXE-NOTFOUND")
-		message(FATAL_ERROR "xmake.exe not found in PATH. Please install it or set XMAKE_EXE.")
-	else()
-		message(STATUS "Found xmake at: ${XMAKE_EXE}")
-	endif()
-
-	include(ExternalProject)
-
-	ExternalProject_Add(
-		CommonLibF4Build
-		SOURCE_DIR ${CommonLibFullPath}
-		CONFIGURE_COMMAND ""
-		BUILD_COMMAND ${CMAKE_COMMAND} -E env
-			XMAKE_COLORTERM=nocolor
-			${XMAKE_EXE} -y
-		BUILD_IN_SOURCE 1
-		INSTALL_COMMAND ""
-	)
-
-	add_library(${CommonLibTarget} STATIC IMPORTED GLOBAL)
-	add_library(${CommonLibSharedTarget} STATIC IMPORTED GLOBAL)
-
-	set_target_properties(${CommonLibName} PROPERTIES
-		IMPORTED_LOCATION
-			"${CommonLibBuildPath}/commonlibf4.lib"
-		INTERFACE_INCLUDE_DIRECTORIES
-			"${CommonLibFullPath}/include"
-	)
-
-	set_target_properties(${CommonLibSharedName} PROPERTIES
-		IMPORTED_LOCATION
-			"${CommonLibBuildPath}/commonlib-shared.lib"
-		INTERFACE_INCLUDE_DIRECTORIES
-			"${CommonLibSharedPath}/include"
-	)
-
-	add_dependencies(${CommonLibTarget} CommonLibF4Build)
+	include(CommonLibF4)
 endif()
 
 target_include_directories(
