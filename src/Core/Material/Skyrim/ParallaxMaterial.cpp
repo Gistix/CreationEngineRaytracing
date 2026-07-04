708c4d2ba05944b9e4d7e517f33894cb2e6f9f3f
Gistix <gistix.md@hotmail.com>
2026-07-04 19:00:23 -0300

    add hash key to detect reused pointer

--- src/Core/Material/Skyrim/ParallaxMaterial.cpp
+++ src/Core/Material/Skyrim/ParallaxMaterial.cpp
@@ -6,6 +6,7 @@
 ParallaxMaterial::ParallaxMaterial(RE::BSShaderMaterial* shaderMaterial, uint64_t offset)
 {
 	m_Offset = offset;
+	m_HashKey = shaderMaterial->hashKey;
 
 	m_Data = eastl::make_unique<ParallaxMaterialData>();
 
