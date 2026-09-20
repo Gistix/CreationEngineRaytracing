cb8313389e468b32881bc5b6582e4fb6a0ddb2ba
Gistix <gistix.md@hotmail.com>
2026-09-20 19:52:33 -0300

    fix(pathtracing): restore sharc validity check scale

--- shaders/raytracing/Pathtracing/RayGeneration.hlsl
+++ shaders/raytracing/Pathtracing/RayGeneration.hlsl
@@ -1253,7 +1253,7 @@ void Main()
                 materialRoughnessPrev = min(materialRoughnessPrev, 0.99f);
                 float a2 = materialRoughnessPrev * materialRoughnessPrev * materialRoughnessPrev * materialRoughnessPrev;
                 float footprint = payload.hitDistance * sqrt(0.5f * a2 / max(1.0f - a2, DIV_EPSILON));
-                isValidHit &= footprint > voxelSize * M_TO_GAME_UNIT;
+                isValidHit &= footprint > voxelSize;
                 isValidHit &= material.Feature != Feature::kHairTint;
             }
 
